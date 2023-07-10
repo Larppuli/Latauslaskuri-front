01e9dc6e22a3c00dbd0f1fc31e753c8646a509e2
Lauri Talvitie <lauri.talvitie@hotmail.com>
2023-07-10 17:26:53 +0300

    Latausnapin luominen

---
+++ src/Components/Tiedostolataus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tiedostolataus() {
+
+  return (
+    <div>
+        <button className='nappi2'>Lataa tiedosto</button>
+    </div>
+  );
+}
+
+export default Tiedostolataus;
