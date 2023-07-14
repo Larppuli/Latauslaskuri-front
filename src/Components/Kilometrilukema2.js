19b047b3fbef7395d97a4a37a5d9667f66b36884
Lauri Talvitie <lauri.talvitie@hotmail.com>
2023-07-14 16:56:16 +0300

    Komponentin luonti

---
+++ src/Components/Kilometrilukema2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Kilometrilukema2({ onKilometerChange }) {
+
+  const handleKilometerChange = (event) => {
+    const kilometer = parseInt(event.target.value);
+    onKilometerChange(kilometer);
+  };
+
+  return (
+    <div className="div2">
+        <input className="input4" type="number" step="1" onChange={handleKilometerChange} placeholder="Kilometrilukema latauksen jälkeen" />
+    </div>
+  );
+}
+
+export default Kilometrilukema2;
