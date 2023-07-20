d654d092eacc50b3b710c6c129904b04a1e96328
Lauri Talvitie <lauri.talvitie@hotmail.com>
2023-07-20 12:44:35 +0300

    ...

--- src/Components/Ajoaika.js
+++ src/Components/Ajoaika.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Ajoaika({ selectedStartingTime, selectedEndingTime, onStartingTimeChange, onEndingTimeChange }) {
-
   const handleStartingTimeChange = (event) => {
     const selectedTime = event.target.value;
     onStartingTimeChange(selectedTime);
